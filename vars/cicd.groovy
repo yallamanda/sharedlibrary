62021640d48735994a2df9cafac9d009848668ec
yallamanda <yallamanda2020@gmail.com>
2022-11-25 12:03:17 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -11,3 +11,10 @@ def newMaven()
   sh 'mvn package'
 }
 
+def newDeploy(jobname,ip,context)
+
+{
+  sh 'scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war  ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war'
+  
+}
+

13cba026ae283edb53b7daeb2e5b990b2ef0f04e
yallamanda <yallamanda2020@gmail.com>
2022-11-25 10:26:44 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -1,5 +1,27 @@
-def call(name)
+def sum(a,b)
+
 {
-  echo "Hey ${name},How are you"
+echo "The sum of ${a} and ${b} is ${a+b}"
+
+}
+
+def sub(a,b)
+
+{
+echo "The subtraction of ${a} and ${b} is ${a-b}"
+
+}
+
+def mul(a,b)
+
+{
+echo "The product of ${a} and ${b} is ${a*b}"
+
+}
+
+def div(a,b)
+
+{
+echo "The division of ${a} and ${b} is ${a/b}"
 
 }
